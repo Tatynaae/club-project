047517eeefd4f4e7dad1466899434a2eb4ba03a3
Ademir__DEV <114341527+ademir06@users.noreply.github.com>
2023-01-12 15:11:45 +0600

    Ademir (#2)
    * Ademir
    
    * ademir
    
    Co-authored-by: Tatyna <111748254+tatynaDev@users.noreply.github.com>

---
+++ src/Components/Hearoes/AdemirLevel/AdemirLevel.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+const AdemirLevel = () => {
+    return (
+        <div>
+            <div id="level">
+                <div className="container">
+                    <div className="level">
+                        <div className="level--text">
+                            <h1>Уровень пакетов</h1>
+                            <p>Списания будут автоматическими. Вы всегда можете отменить подписку в <br/> Вашем личном
+                                кабинете и больше списаний не будет
+                            </p>
+                        </div>
+                        <div className="level--block">
+                            <div className="level--block--one">
+                                <div className="level--block--one--info">
+                                    <div className="level--block--one--info__text">
+                                        <h2>Месяц +</h2>
+                                        <p>Доступны: Все статьи</p>
+                                    </div>
+                                    <div className="level--block--one--info__som">
+                                        <h1>105,00 $</h1>
+                                        <p>Ежемесячно</p>
+                                    </div>
+                                    <div className="level--block--one--info__btn">
+                                        <div className="level--block--one--info__btn--one">
+                                            <button>Оформить подписку</button>
+                                        </div>
+                                        <div className="level--block--one--info__btn--two">
+                                            <button> Подробнее</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="level--block--one">
+                                <div className="level--block--one--info">
+                                    <div className="level--block--one--info__text">
+                                        <h2>Год +</h2>
+                                        <p>Доступны: Все Мастер классы  и <br/> статьи и курсы</p>
+                                    </div>
+                                    <div className="level--block--one--info__som">
+                                        <h1>585,00 $</h1>
+                                        <p>Ежегодно</p>
+                                    </div>
+                                    <div className="level--block--one--info__btn">
+                                        <div className="level--block--one--info__btn--one">
+                                            <button>Оформить подписку</button>
+                                        </div>
+                                        <div className="level--block--one--info__btn--two">
+                                            <button> Подробнее</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="level--block--one">
+                                <div className="level--block--one--info">
+                                    <div className="level--block--one--info__text">
+                                        <h2>Год </h2>
+                                        <p>Доступны: Все Мастер классы и <br/>статьи и курсы</p>
+                                    </div>
+                                    <div className="level--block--one--info__som">
+                                        <h1>220,00 $</h1>
+                                        <p>Ежегодно</p>
+                                    </div>
+                                    <div className="level--block--one--info__btn">
+                                        <div className="level--block--one--info__btn--one">
+                                            <button>Оформить подписку</button>
+                                        </div>
+                                        <div className="level--block--one--info__btn--two">
+                                            <button> Подробнее</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdemirLevel;
---
+++ src/Components/Hearoes/AdemirSection/AdemirSection.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {BsArrowRight} from "react-icons/bs";
+const AdemirSection = () => {
+    return (
+            <section id="section">
+                <div className="container">
+                    <div className="section">
+                        <div className="section--text">
+                            <h1>Курсы</h1>
+                        </div>
+                        <div className="section--block">
+                            <div className="section--block__one">
+                                <h1>Frontend-разработчик</h1>
+                                <p>
+                                    Мастер создания сайтов. Умеет делать их красивыми, интерактивными, с большим
+                                    функционалом. Профессия отлично подойдет тем, кто хочет фрилансить и постоянно
+                                    разрабатывать новые проекты
+                                </p>
+                                <div className="section--block__one__btn">
+                                    <button>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                                </div>
+                            </div>
+                            <div className="section--block__one">
+                                <h1>Backend-разработчик</h1>
+                                <p>
+                                    Специалист, который отвечает за построение логики для воплощения любой идеи. Он
+                                    собирает фундамент и опорную систему для проекта - от простого
+                                    сайта для магазина одежды до сложных вычислительных систем нейронных сетей.
+                                </p>
+                                <div className="section--block__one__btn">
+                                    <button>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                                </div>
+                            </div>
+                            <div className="section--block__one">
+                                <h1>UX / UI Дизайнер</h1>
+                                <p>
+                                    Креативный специалист, который придумывает дизайн и интерфейс продукта.
+                                    Специалист UX/UI отвечает за подбор форм, цветов, Функциональности дизайна и прочих
+                                    важных параметров для комфорьного использования продукта.
+                                </p>
+                                <div className="section--block__one__btn">
+                                    <button>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="section--btn">
+                            <button>Все курсы</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    );
+};
+
+export default AdemirSection;
