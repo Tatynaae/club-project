7f10ff182dff67eca6eec697c003b7f549f4a4bd
ademir06 <gaparov866@gmail.com>
2023-01-24 11:45:37 +0600

    asdv

--- src/Components/Courses/Course.js
+++ src/Components/Courses/Course.js
@@ -10,161 +10,129 @@ const Course = () => {
                     <div className="course--text">
                         <h1>Курсы</h1>
                     </div>
-                    <div className="course__block">
-                        <div className="course__block--one">
-                            <h1>Frontend-разработчик</h1>
+                    <div className='course--blocks'>
+                        <div className="course--blocks__block">
+                            <h2>Frontend-разработчик</h2>
                             <p>
                                 Мастер создания сайтов. Умеет делать их красивыми, интерактивными, с большим
                                 функционалом. Профессия отлично подойдет тем, кто хочет фрилансить и постоянно
                                 разрабатывать новые проекты
                             </p>
-                            <div className="course--block__one--btn">
-                                <NavLink to={'/Подробнее'}>
-                                    <button style={{
-                                        border: 'none',
-                                        background: 'none',
-                                        marginTop: '20px',
-                                        borderBottom: '2px solid black',
-                                        color: '#000000'
-                                    }}>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                            <div className="course--blocks__block--group">
+                                <NavLink>
+                                    Подробнее
                                 </NavLink>
+                                <BsArrowRight className='course--blocks__block--group__icons'/>
                             </div>
                         </div>
-                        <div className="course__block--one">
-                            <h1>Backend-разработчик</h1>
+                        <div className="course--blocks__block">
+                            <h2>Frontend-разработчик</h2>
                             <p>
-                                Специалист, который отвечает за построение логики для
-                                воплощения любой идеи. Он собирает фундамент и опорную систему для проекта - от простого сайта для магазина
-                                одежды до сложных вычислительных систем нейронных сетей.
+                                Специалист, который отвечает за построение логики для воплощения любой идеи. Он собирает
+                                фундамент и опорную систему для проекта - от простого сайта для магазина одежды до
+                                сложных вычислительных систем нейронных сетей.
                             </p>
-                            <div className="course--block__one--btn">
-                                <button style={{
-                                    border: 'none',
-                                    background: 'none',
-                                    marginTop: '20px',
-                                    borderBottom: '2px solid black',
-                                    color: '#000000'
-                                }}>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                            <div className="course--blocks__block--group">
+                                <NavLink>
+                                    Подробнее
+                                </NavLink>
+                                <BsArrowRight className='course--blocks__block--group__icons'/>
                             </div>
                         </div>
-                        <div className="course__block--one">
-                            <h1>UX / UI Дизайнер</h1>
+                        <div className="course--blocks__block">
+                            <h2>Frontend-разработчик</h2>
                             <p>
-                                Креативный специалист, который придумывает дизайн и интерфейс продукта.
-                                Специалист UX/UI отвечает за подбор форм, цветов, Функциональности дизайна
-                                и прочих важных параметров для комфорьного использования продукта.
+                                Креативный специалист, который придумывает дизайн и интерфейс продукта. Специалист UX/UI
+                                отвечает за подбор форм, цветов, Функциональности дизайна и прочих важных параметров для
+                                комфорьного использования продукта.
                             </p>
-                            <div className="course--block__one--btn">
-                                <button style={{
-                                    border: 'none',
-                                    background: 'none',
-                                    marginTop: '20px',
-                                    borderBottom: '2px solid black',
-                                    color: '#000000'
-                                }}>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                            <div className="course--blocks__block--group">
+                                <NavLink>
+                                    Подробнее
+                                </NavLink>
+                                <BsArrowRight className='course--blocks__block--group__icons'/>
                             </div>
                         </div>
-                    </div>
-                    <div className="course__block">
-                        <div className="course__block--one">
-                            <h1>DevOps - инженер</h1>
+                        <div className="course--blocks__block">
+                            <h2>Frontend-разработчик</h2>
                             <p>
-                                Он разбирается в программировании и инфраструктуре
-                                и объединяет эти знания для оптимальной работы бизнеса.
+                                Он разбирается в программировании и инфраструктуре и объединяет эти знания для
+                                оптимальной работы бизнеса.
                             </p>
-                            <div className="course--block__one--btn">
-                                <button style={{
-                                    border: 'none',
-                                    background: 'none',
-                                    marginTop: '20px',
-                                    borderBottom: '2px solid black',
-                                    color: '#000000'
-                                }}>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                            <div className="course--blocks__block--group">
+                                <NavLink>
+                                    Подробнее
+                                </NavLink>
+                                <BsArrowRight className='course--blocks__block--group__icons'/>
                             </div>
                         </div>
-                        <div className="course__block--one">
-                            <h1>Android-разработчик</h1>
+                        <div className="course--blocks__block">
+                            <h2>Frontend-разработчик</h2>
                             <p>
-                                Создает, обновляет, усовершенствует мобильные приложения для смартфонов,
-                                планшетов, электронных книг, игровых приставок
-                                и других девайсов, работающих на операционной системе Android.
+                                Создает, обновляет, усовершенствует мобильные приложения для смартфонов, планшетов,
+                                электронных книг, игровых приставок и других девайсов, работающих на операционной
+                                системе Android.
                             </p>
-                            <div className="course--block__one--btn">
-                                <button style={{
-                                    border: 'none',
-                                    background: 'none',
-                                    marginTop: '20px',
-                                    borderBottom: '2px solid black',
-                                    color: '#000000'
-                                }}>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                            <div className="course--blocks__block--group">
+                                <NavLink>
+                                    Подробнее
+                                </NavLink>
+                                <BsArrowRight className='course--blocks__block--group__icons'/>
                             </div>
                         </div>
-                        <div className="course__block--one">
-                            <h1>Java-разработчик</h1>
+                        <div className="course--blocks__block">
+                            <h2>Frontend-разработчик</h2>
                             <p>
-                                Cпециалист, который создает приложения разной сложности, используя старейший язык программирования.
+                                Cпециалист, который создает приложения разной сложности, используя старейший язык
+                                программирования.
 
                             </p>
-                            <div className="course--block__one--btn">
-                                <button style={{
-                                    border: 'none',
-                                    background: 'none',
-                                    marginTop: '20px',
-                                    borderBottom: '2px solid black',
-                                    color: '#000000'
-                                }}>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                            <div className="course--blocks__block--group">
+                                <NavLink>
+                                    Подробнее
+                                </NavLink>
+                                <BsArrowRight className='course--blocks__block--group__icons'/>
                             </div>
                         </div>
-                    </div>
-                    <div className="course__block">
-                        <div className="course__block--one">
-                            <h1>С++ - разработчик</h1>
+                        <div className="course--blocks__block">
+                            <h2>Frontend-разработчик</h2>
                             <p>
-                                Занимается разработкой высокопроизводительных и высоконагруженных
-                                систем, таких как поисковики, драйверы, игры и приложения.
+                                Занимается разработкой высокопроизводительных и высоконагруженных систем, таких как
+                                поисковики, драйверы, игры и приложения.
                             </p>
-                            <div className="course--block__one--btn">
-                                <button style={{
-                                    border: 'none',
-                                    background: 'none',
-                                    marginTop: '20px',
-                                    borderBottom: '2px solid black',
-                                    color: '#000000'
-                                }}>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                            <div className="course--blocks__block--group">
+                                <NavLink>
+                                    Подробнее
+                                </NavLink>
+                                <BsArrowRight className='course--blocks__block--group__icons'/>
                             </div>
                         </div>
-                        <div className="course__block--one">
-                            <h1>IOS - разработчик</h1>
+                        <div className="course--blocks__block">
+                            <h2>Frontend-разработчик</h2>
                             <p>
-                                iOS разработчик - идеальная профессия для того, кто любит мобильную продукцию компании Apple. Специальность позволяет совмещать творческие способности.
+                                iOS разработчик - идеальная профессия для того, кто любит мобильную продукцию компании
+                                Apple. Специальность позволяет совмещать творческие способности.
 
                             </p>
-                            <div className="course--block__one--btn">
-                                <button style={{
-                                    border: 'none',
-                                    background: 'none',
-                                    marginTop: '20px',
-                                    borderBottom: '2px solid black',
-                                    color: '#000000'
-                                }}>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                            <div className="course--blocks__block--group">
+                                <NavLink>
+                                    Подробнее
+                                </NavLink>
+                                <BsArrowRight className='course--blocks__block--group__icons'/>
                             </div>
                         </div>
-                        <div className="course__block--one">
-                            <h1>Python - разработчик</h1>
+                        <div className="course--blocks__block">
+                            <h2>Frontend-разработчик</h2>
                             <p>
                                 Cпециалист, который применяет в работе многоуровневый язык программирования Python.
                             </p>
-                            <div className="course--block__one--btn">
-                                <button style={{
-                                    border: 'none',
-                                    background: 'none',
-                                    marginTop: '20px',
-                                    borderBottom: '2px solid black',
-                                    color: '#000000'
-                                }}>Подробнее <BsArrowRight style={{background: 'none'}}/></button>
+                            <div className="course--blocks__block--group">
+                                <NavLink>
+                                    Подробнее
+                                </NavLink>
+                                <BsArrowRight className='course--blocks__block--group__icons'/>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
